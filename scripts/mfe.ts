f1c0b5681a8b62b27f50304f516b6c2345408646
JC <jeanchristophe.nicolas@vodafone.com>
2024-01-25 15:26:27 +0000

    add header app

--- scripts/mfe.ts
+++ scripts/mfe.ts
@@ -2,7 +2,7 @@ import federation from "@originjs/vite-plugin-federation";
 export default () => federation({
     name: 'host-app',
     remotes: {
-        // header: "https://rude-clouds.surge.sh/assets/remoteEntry.js",
+        header: "https://unnatural-slope.surge.sh/assets/remoteEntry.js",
         catalogue: "https://true-robin.surge.sh/assets/remoteEntry.js",
         basket: "https://seemly-temper.surge.sh/assets/remoteEntry.js",
     },
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import './style.css'
 
-// const header:any = () => import("header/App");
+const header:any = () => import("Header/App");
 const catalogue:any = () => import("catalogue/App");
 const basket:any = () => import("basket/App");
 
@@ -11,10 +11,6 @@ document.querySelector<HTMLDivElement>('#title')!.innerHTML = `
   </div>
 `
 
-// header().then(fe => fe.default(document.getElementById("header"))).catch(() => console.log("issue with loading header"))
-catalogue().then(app => {
-
-  console.log(app.default)
-  app.default("catalogue")
-}).catch((e) => console.log("issue with loading catalogue", e))
+header().then(fe => fe.default(document.getElementById("header"))).catch(() => console.log("issue with loading header"))
+catalogue().then(app => app.default("catalogue")).catch((e) => console.log("issue with loading catalogue", e))
 basket().then(fe => fe.default("basket")).catch(() => console.log("issue with loading basket"))
