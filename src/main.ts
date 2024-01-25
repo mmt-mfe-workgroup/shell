91d9268d8f871d14a1c323a2c87ca2faf3752ab2
JC <jeanchristophe.nicolas@vodafone.com>
2024-01-25 15:48:15 +0000

    update colour

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ const basket:any = () => import("basket/App");
 
 document.querySelector<HTMLDivElement>('#title')!.innerHTML = `
   <div>
-    <h1>Zain's Phonez</h1>
+    <h1>Zain's Phonez 2000</h1>
   </div>
 `
 
-header().then(fe => fe.default(document.getElementById("header"))).catch(() => console.log("issue with loading header"))
+header().then(fe => fe.default("header")).catch(() => console.log("issue with loading header"))
 catalogue().then(app => app.default("catalogue")).catch((e) => console.log("issue with loading catalogue", e))
 basket().then(fe => fe.default("basket")).catch(() => console.log("issue with loading basket"))
