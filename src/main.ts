e682f9f4b6d3b21045b193e3c8453bc9852aed88
JC <jeanchristophe.nicolas@vodafone.com>
2024-01-26 15:21:30 +0000

    create register module subcription

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 // @ts-nocheck
+import ModuleSync from './pubsub'
 import './style.css'
+
+window.__MFE_SYNC__ = new ModuleSync(['header', 'catalogue', 'basket'])
 
 const header:any = () => import("header/App");
 const catalogue:any = () => import("catalogue/App");
@@ -7,13 +10,22 @@ const basket:any = () => import("basket/App");
 
 document.querySelector<HTMLDivElement>('#title')!.innerHTML = `
   <div>
-    <h1>Zain's Phonez 2000</h1>
+    <h1>MMT MFE SHELL</h1>
   </div>
 `
-
 header().then(fe => {
-  console.log(fe)
   fe.default(document.getElementById("header"))
+  const [subscription, targetEvent, ready] = __MFE_SYNC__.subscribe('header', () => console.log('sync please'))
+  window.addEventListener(subscription, (e) => console.log("can sync this..", e))
+  // ready()
+  console.log(targetEvent)
+  setTimeout(() => {
+    const syncState = new CustomEvent(targetEvent, {
+      detail: { show: true},
+    });
+    console.log(syncState)
+    window.dispatchEvent(syncState)
+  }, 500)
 }).catch(() => console.log("issue with loading header"))
 catalogue().then(app => app.default("catalogue")).catch((e) => console.log("issue with loading catalogue", e))
 basket().then(fe => fe.default("basket")).catch(() => console.log("issue with loading basket"))
---
+++ src/pubsub.ts
@@ -0,0 +1,69 @@
+const STORAGE = "MFE"
+const SYNC_OUT_EVENT = "MFE_SYNC"
+const CUSTOM_LISTENER = "MFE_STORE"
+export class ModuleSync {
+    private modules: any[]
+    private hasStorage: boolean
+    public custom: CustomEvent
+    constructor(expected: string[]) {
+        this.modules = []
+        this.hasStorage = !!localStorage.getItem(STORAGE)
+        this.custom = new CustomEvent(SYNC_OUT_EVENT, {detail: { store: localStorage.getItem(STORAGE) }})
+        this.initialise(expected)
+    }
+    register(name: string) {
+        this.modules.push(this.prepare(name))
+    }
+    registerListener(event: string) {
+        const useEventHandler = this.setupReaction(event)
+        window.addEventListener(event, useEventHandler)
+    }
+    initialise(list: string[]) {
+        this.modules = list.map((name: string) => this.prepare(name))
+    }
+    prepare(name: string) {
+        const store = `${CUSTOM_LISTENER}_${name.toUpperCase()}`    
+        return {
+            name,
+            state: "REGISTERED",
+            sync: this.hasStorage ? "PENDING" : "OMIT",
+            callback: null,
+            store,
+        }
+    }
+    subscribe(name:string, callback: () => void) {
+        let SYNC_IN_EVENT
+        this.modules = this.modules.map(module => {
+            if(name === module.name) {
+                module.state = "MOUNTED"
+                module.callback = callback
+                SYNC_IN_EVENT = module.store
+                this.registerListener(SYNC_IN_EVENT)
+            }
+            return module
+        })
+        
+        return [SYNC_OUT_EVENT, SYNC_IN_EVENT, this.checkForBroadcast]
+    }
+    checkForBroadcast() {
+        // const canBroadcast = this.modules.filter(module => module.state === 'MOUNTED').length === this.modules.length
+        window.dispatchEvent(this.custom)
+    }
+    setupReaction(eventName) {
+        const [{ name }] = this.modules.filter(module => {
+            return eventName.toLowerCase().includes(module.name)
+        })
+        return ({ detail }: CustomEvent) => {
+            const store = localStorage.getItem(STORAGE) ?? "{}"
+            const update = JSON.parse(store)
+            update[name] = {...detail }
+            console.log(update)
+            localStorage.setItem(STORAGE, JSON.stringify(update))
+        }
+    }
+    clearStore() {
+        localStorage.clear()
+    }
+}
+
+export default ModuleSync
