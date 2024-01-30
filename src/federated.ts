a49b8f603b83f4318a45d47880bdd5211aa643d2
JC <jeanchristophe.nicolas@vodafone.com>
2024-01-30 15:16:03 +0000

    create federated loader

---
+++ src/federated.ts
@@ -0,0 +1,28 @@
+// @ts-nocheck
+import MODULE_DATA from './config'
+
+export function federatedLoader(key, callback) {
+    const { app, element, props } = MODULE_DATA[key]
+    app.then(module => {
+        module.default(element, {...props})
+        callback?.()
+    }).catch((e) => console.log(`issue with loading ${key}`, e))
+}
+
+export function mount(app) {
+    // pass into FM to register module ^^
+  const [subscription, targetEvent, ready] = __MFE_SYNC__.subscribe(app)
+  console.log(subscription, targetEvent)
+  // inside FM listen to sync refresh event
+  window.addEventListener(subscription) // callback to assign sync
+  // let other federated apps know it's go time
+  ready()
+  // example save new state
+  // setTimeout(() => {
+  //   const syncState = new CustomEvent(targetEvent, {
+  //     detail: { show: true},
+  //   });
+  //   console.log(syncState)
+    // window.dispatchEvent(syncState)
+  // }, 500)
+}
