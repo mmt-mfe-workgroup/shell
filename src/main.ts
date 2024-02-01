0d3017c7d88b9643b1d526cb39526768c212aecb
JC <jeanchristophe.nicolas@vodafone.com>
2024-02-01 15:59:46 +0000

    add x-ray mode

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
 // @ts-nocheck
 import ModuleSync from './pubsub'
 import { federatedLoader } from './federated';
+import initEventListeners from './runEventListeners'
 import './style.css'
-
-// useless mounting
-document.querySelector<HTMLDivElement>('#title')!.innerHTML = `<div class="shell-header">
-  <h1>MMT MFE SHELL</h1>
-  <div class="flex items-center mb-4">
-    <input id="mfe-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-    <label for="mfe-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">MFE x-ray mode</label>
-  </div>
-  <code>
-  CDN_LOCATION = https://unnatural-slope.surge.sh/assets/remoteEntry.js
-  FRAMEWORK = REACT@18.2
-  APP NAME AND VERSION = SHELL
-  SHARED RESOURCES = N/A
-  EVENTS - DISPATCHED => "XRAY"
-  EVENTS - SUBSCRIBED => "addToBasket"
-  <code>
-</div>`
 
 const onLoadApps = ['header', 'catalogue', 'basket']
 
@@ -27,48 +11,27 @@ window.__MFE_SYNC__ = new ModuleSync(onLoadApps)
 // load Apps
 onLoadApps.forEach(app => federatedLoader(app))
 
-// verbose [event] handling for checkout init
+// useless mounting
+document.querySelector<HTMLDivElement>('#title')!.innerHTML = `
+<div class="ui">
+  <h1>MMT MFE SHELL</h1>
+</div>
+<div class="flex items-center mb-4">
+    <input id="mfe-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+    <label for="mfe-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">MFE x-ray mode</label>
+  </div>
+<code>
+APP: <span class="tag">mfe-shell@0.0.1</span>
+<br>
+CDN_LOCATION: <span class="tag">unnatural-slope.surge.sh/assets/remoteEntry.js</span>
+<br>
+FRAMEWORK: <span class="tag">React@18.2</span>
+<br>
+SHARED_RESOURCES: <span class="tag">N/A</span>
+<br>
+EVENTS:DISPATCHED => <span class="tag mfe-blue">"view:x-ray"</span>
+<br>
+EVENTS:SUBSCRIBED => <span class="tag mfe-yellow">"goToCheckout"</span> <span class="tag mfe-brown">"clearBasket"</span>
+</code>`
 
-// switch view to basket|checkout
-window.addEventListener('goToCheckout', () => {
-  const tgt = document.getElementById("checkout")
-  const cat = document.getElementById("accordion")
-  const bas = document.getElementById("basket")
-  tgt?.classList.remove('hidden')
-  tgt?.classList.add('flex-1')
-  cat?.classList.add('hidden')
-  bas?.classList.add('flex-1')
-  if(tgt?.classList.contains('init')) {
-    federatedLoader('checkout', () => {
-      window.__MFE_SYNC__.register('checkout')
-      tgt?.classList.remove('init')
-    })
-  }
-})
-
-// will revert view back to catalogue|basket
-window.addEventListener('clearBasket', () => {
-  const tgt = document.getElementById("checkout")
-  if(tgt?.classList.contains('flex-1')) {
-    const cat = document.getElementById("accordion")
-    const bas = document.getElementById("basket")
-    tgt?.classList.add('hidden')
-    tgt?.classList.remove('flex-1')
-    cat?.classList.remove('hidden')
-    bas?.classList.remove('flex-1')
-  }
-})
-
-// setup x-ray event dispatcher
-const shell = document.getElementById('title')
-const checkbox = document.getElementById('mfe-checkbox')
-checkbox?.addEventListener('change', ({ target }) => {
-  console.log('view:x-ray', target.checked)
-  window.dispatchEvent(new CustomEvent('view:x-ray', { detail: target.checked }))
-  if(target.checked) {
-    shell?.classList.add('x-ray')
-  } else {
-    shell?.classList.remove('x-ray')
-  }
-})
-
+initEventListeners()
