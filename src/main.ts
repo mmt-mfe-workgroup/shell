b44336b0b8024216a1aad02efb92281a14417e6a
JC <jeanchristophe.nicolas@vodafone.com>
2024-02-02 09:45:38 +0000

    fix styles

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ document.querySelector<HTMLDivElement>('#title')!.innerHTML = `
   <h1>MMT MFE SHELL</h1>
 </div>
 <div class="flex items-center mb-4">
-    <input id="mfe-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+    <input id="mfe-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-900 rounded focus:ring-blue-500 focus:ring-2">
     <label for="mfe-checkbox" class="ms-2 text-sm font-medium text-gray-900">MFE x-ray mode</label>
   </div>
 <code>
